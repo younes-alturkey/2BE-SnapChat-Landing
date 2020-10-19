5cfafc42ad502e891c85e5e06cf48dd9745fa186
Younes Alturkey <alturkeyy@gmail.com>
2020-10-19 16:22:11 +0300

    Updates

---
+++ precache-manifest.37c4a2754823766e66124f87002304e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e26ff1bb49e8ffeb84796663c7e2c4e",
+    "url": "/2BE-SnapChat-Landing/index.html"
+  },
+  {
+    "revision": "62af76e67bd56883fdd3",
+    "url": "/2BE-SnapChat-Landing/static/css/2.a7eea44f.chunk.css"
+  },
+  {
+    "revision": "62af76e67bd56883fdd3",
+    "url": "/2BE-SnapChat-Landing/static/js/2.4a350965.chunk.js"
+  },
+  {
+    "revision": "f2250deb42e7c0e00f75f8a622eeb038",
+    "url": "/2BE-SnapChat-Landing/static/js/2.4a350965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfaa7260df37883f1ec",
+    "url": "/2BE-SnapChat-Landing/static/js/main.464e9e08.chunk.js"
+  },
+  {
+    "revision": "aaefda75e710e4f04906",
+    "url": "/2BE-SnapChat-Landing/static/js/runtime-main.136d9f97.js"
+  },
+  {
+    "revision": "a1dbb1126f616aaf35ce498eb003cbf9",
+    "url": "/2BE-SnapChat-Landing/static/media/logo.a1dbb112.png"
+  }
+]);
